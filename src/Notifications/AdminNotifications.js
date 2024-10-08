151d75166d06f7dd9f64dcdd9412eaeb34c7ff9c
Đông Nghi <82652596+nghihvd@users.noreply.github.com>
2024-10-08 17:05:38 +0700

    Update AdminNotifications.js

--- src/Notifications/AdminNotifications.js
+++ src/Notifications/AdminNotifications.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from "../services/axios";
 import "../styles/adminpage.scss";
 import { toast } from 'react-toastify';
